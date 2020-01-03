e752191c4b0b15d6b3bcc440485e4937db296865
hfafin <58565156+hfafin@users.noreply.github.com>
2020-01-03 21:27:38 +0100

    Loader Audio

--- loader.py
+++ loader.py
@@ -2,7 +2,9 @@ import torch
 import torchaudio
 import torchaudio.transforms
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
+import datasets
 
 class AudioLoader():
     '''Audio dataset loader'''
@@ -20,10 +22,19 @@ class AudioLoader():
                 n_mels=92)(x),
         #transforms.ToTensor(),
         # crop the end
-        lambda x: x[:,:,:42],
         transforms.Normalize([0], [1]),
         ])
         
+    def loader(self, root_dir, batch_size=1):
+        """
+        Args:
+            - root_dir (torch.utils.data.Dataset): audio dataset path.
+            - batch_size (int) : loading batch size.
+        """
+        dataset = datasets.audioDataset(root_dir, transform=self.preproc)      
+        loader = DataLoader(dataset, batch_size=batch_size)
+        return loader
+         
     def getYESNOdata(self):
         '''Return data from the YESNO database'''
         data = torchaudio.datasets.YESNO(
@@ -38,6 +49,32 @@ class AudioLoader():
         loader = torch.utils.data.DataLoader(data, batch_size=1)
         return loader
     
+    def split(self, spectro, name, max_time = 42, export_dir = 'db/splitAudio/'):
+        length_sp = spectro.shape()[2]
+        n_snips = length_sp // max_time
+        for i in range(n_snips):
+            CropSpectro = transforms.functional.crop(spectro,1,i*42,92,42)
+            snip = transforms.ToTensor(CropSpectro)
+            torch.save(snip, export_dir + name + '_' + str(i) + '.pt')    
+        return None
+    
+    def splitData(self,root,max_time = 42,export_dir = 'db/splitAudio'):
+        audio_loader = self.loader(root)
+        for spectro, name in audio_loader:
+            self.split(
+                    spectro,
+                    name[0],
+                    max_time=max_time,
+                    export_dir=export_dir
+                    )
+            print(name, 'splitted and exported.')
+        return None
+        
+    def audio_snippets_loader(self, batch_size=1, root_dir='db/splitAudio'):
+        """ Audio snippets tensors loader """
+        dataset = datasets.Snippets(root_dir)
+        loader  = DataLoader(dataset, batch_size=batch_size)
+        return loader
     
 class MIDILoader():
     '''MIDI file loader'''
